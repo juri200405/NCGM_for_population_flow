3f51d5dd07d0ccd8ffa597238c6da316ba08d4f3
juri200405 <juri200405@gmail.com>
2019-07-23 12:25:43 +0900

    旧損失関数の除去

--- src/model.py
+++ src/model.py
@@ -52,19 +52,3 @@ class NCGM_objective(nn.Module):
         G = obj_L.add(-1 * lam, et.add(1, et1).sum())
 
         return G.neg()
-    
-    def true_forward(self, theta, yt, yt1, lam):
-        Z = theta.mul(yt.unsqueeze(1)).log().clamp(min=-104.0)
-
-        log_theta = theta.log().clamp(min=-104.0)
-        log_theta_add_1 = log_theta.add(1)
-        Ls_right = log_theta_add_1.add(-1, Z)
-
-        L = Z.exp().mul(Ls_right).sum(1)
-
-        et = yt.add(-1, Z.exp().sum(1)).pow(2)
-        et1 = yt1.add(-1, Z.exp().sum(1)).pow(2)
-
-        G = L.add(-1 * lam, et.add(1, et1))
-
-        return G.sum().neg()
